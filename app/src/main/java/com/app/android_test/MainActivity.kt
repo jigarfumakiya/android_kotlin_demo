f2a33ea0cb0924418e0508d24a746962b4aa7b81
Jigar Fumakiya <jigarfumakiya@gmail.com>
2024-12-01 05:45:30 +0530

    Setup project.

---
+++ app/src/main/java/com/app/android_test/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.app.android_test
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
