abec4774051cd1a954921e960d34d52971f561f8
Jigar Fumakiya <jigarfumakiya@gmail.com>
2024-12-01 15:40:11 +0530

    Added dependencies .

--- app/src/main/java/com/app/android_test/TestAndroidApplication.kt
+++ app/src/main/java/com/app/android_test/TestAndroidApplication.kt
@@ -1,11 +1,13 @@
 package com.app.android_test
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
 /**
  * @Author: Jigar Fumakiya
  * @Date: 01/12/24
  */
 
+@HiltAndroidApp
 class TestAndroidApplication: Application() {
 }
--- app/src/main/java/com/app/android_test/ui/MainActivity.kt
+++ app/src/main/java/com/app/android_test/ui/MainActivity.kt
@@ -2,11 +2,16 @@ package com.app.android_test.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.app.android_test.databinding.ActivityMainBinding
+import com.app.android_test.helpers.binding.viewBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private val binding by viewBinding(ActivityMainBinding::inflate)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
